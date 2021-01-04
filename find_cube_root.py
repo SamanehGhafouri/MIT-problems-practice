0f4c4b3f0d9c5ba8a88fe68909dc5e8bb668d4c0
SamanehGhafouri <samaneh2010gh@gmail.com>
2021-01-03 22:39:17 -0500

    find the cube of perfect cube with while and for loops

--- find_cube_root.py
+++ find_cube_root.py
@@ -8,3 +8,18 @@ else:
     if x < 0:
         ans = - ans
     print('Cube root of ' + str(x) + ' is ' + str(ans))
+
+# -------------------------------------------------------------#
+
+x = int(input('Enter an integer: '))
+for ans in range(0, abs(x) + 1):
+    if ans ** 3 == abs(x):
+        break
+if ans ** 3 != abs(x):
+    print(x, ' is not a perfect cube')
+else:
+    if x < 0:
+        ans =- ans
+    print('Cube root of ' + str(x) + ' is ' + str(ans))
+
+
